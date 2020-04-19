ab1ced56ed15acad085772c8730fd518a11da4fa
z826540272 <56025847+z826540272@users.noreply.github.com>
2020-04-19 15:15:05 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,8 +7,9 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
-mkdir package/base-files/files/config
-echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+# mkdir package/base-files/files/config
+# echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+rm -rf package/lean/smartdns
 git clone https://github.com/z826540272/openwrt-smartdns.git feeds/packages/net/smartdns
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
