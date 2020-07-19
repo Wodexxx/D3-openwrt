b6adf211e77d36565d609a848e4d8fd8dd938073
z826540272 <56025847+z826540272@users.noreply.github.com>
2020-07-19 19:12:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,5 @@ sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_gener
 # mkdir package/base-files/files/config
 # echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 rm -rf package/lean/smartdns
-# git clone https://github.com/z826540272/openwrt-smartdns.git package/smartdns
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
-git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-# git clone https://github.com/kenzok8/small.git package/small
-
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
