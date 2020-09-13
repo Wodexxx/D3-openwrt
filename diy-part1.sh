7dd85b2f67ecabee6e953de8b3836bc601421777
w8951858 <w8951858@163.com>
2020-09-13 19:59:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 # mkdir package/base-files/files/config
 # echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 rm -rf package/lean/smartdns
